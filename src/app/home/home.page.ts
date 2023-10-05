031583092bf9d6155d91504b086d4d9ba1c8ffd2
Julian Garcia Velazquez <jgarvelju076@g.educaand.es>
2023-10-05 10:50:41 +0200

    proyecto finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  numSecreto: number = 0;
+  numUsuario: number = 0;
+  txtMensaje: string = "";
+  esCorrecto: boolean= false;
+
+  constructor() {
+    this.generarNumSecreto();
+  }
+
+  comprobarNumero() {
+    console.log("clic en boton");
+    console.log("numUsuario="+this.numUsuario);
+    if(this.numUsuario == this.numSecreto){
+      console.log("¡¡Ha acertado!!");
+      this.txtMensaje = ("¡¡Ha acertado!!");
+      this.esCorrecto = true;
+    } else if(this.numSecreto > this.numUsuario){
+      console.log("numSecreto es mayor")
+      this.txtMensaje = ("El numero secreto es mayor");
+    } else if(this.numSecreto < this.numUsuario){
+      console.log("numSecreto es menor")
+      this.txtMensaje = ("El numero secreto es menor");
+    }
+  }
+
+  generarNumSecreto() {
+    this.numSecreto = Math.floor(Math.random()*100+1);
+    console.log("numsecreto="+this.numSecreto)
+  }
+
+  newNumeroSecreto(){
+    this.generarNumSecreto();  //no olvidar los parentesis al final de los metodos
+    this.esCorrecto=false;
+  }
 
 }
